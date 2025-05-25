85602ad3fe5b2070b18186837ccd54fb64a8b686
hemangsengar <hemang.sengar@gmail.com>
2025-05-26 02:07:59 +0530

    Changing Scraping as per the requirement

--- scrap.py
+++ scrap.py
@@ -3,12 +3,33 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from database import *
 
+SITE_CONFIGS = {
+    'quotes': {
+        'base_url': 'https://quotes.toscrape.com/',
+        'start_page': 'page/1/',
+        'article_selector': 'div.quote',
+        'title_selector': 'span.text',
+        'author_selector': 'small.author',
+        'content_selector': 'span.text',
+        'next_selector': 'li.next a',
+        'type': 'quotes'
+    },
+    'hackernews': {
+        'base_url': 'https://news.ycombinator.com/',
+        'start_page': '',
+        'article_selector': 'tr.athing',
+        'title_selector': 'span.titleline a',
+        'author_selector': 'a.hnuser',
+        'content_selector': 'span.titleline a',
+        'next_selector': 'a.morelink',
+        'type': 'news'
+    }
+}
 
-
-def scrape():
+def scrape_articles():
     base_url = "https://quotes.toscrape.com/"
     url = "page/1/"
-    
+    articles = []
     while url:
         fullURL = urljoin(base_url,url)
         print(f"Scrapping: {fullURL}")
