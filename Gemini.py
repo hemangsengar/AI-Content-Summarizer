2f153ed092933a1d8d7f750ba6ddd3bf6ed67c48
hemangsengar <hemang.sengar@gmail.com>
2025-05-26 16:52:32 +0530

    Basic Gemini Done!

--- Gemini.py
+++ Gemini.py
@@ -1,8 +1,18 @@
-from google import genai
 
-client = genai.Client(api_key="YOUR_API_KEY")
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+import os
+
+# Load the .env file
+load_dotenv()
+api_key = os.getenv("API_KEY")
+
+client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
-    model="gemini-2.0-flash", contents="Explain how AI works in a few words"
+    model="gemini-2.0-flash",
+    contents="Explain how AI works in a few words",
 )
-print(response.text)
+
+print(response)
